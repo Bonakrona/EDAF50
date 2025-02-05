3d164010bfb4d44f68b3965eb947cd963fb26e76
Your Name <you@example.com>
2025-02-05 15:49:47 +0100

    A2 done

--- lab2/IO.cc
+++ lab2/IO.cc
@@ -1,12 +1,3 @@
-// Write a program that reads the file and creates a new file words.txt in the current directory. Each line in
-// the file shall contain a word, the number of trigrams in the word, and the trigrams. The
-// trigrams shall be sorted in alphabetical order; upper case letters shall be changed to lower
-// case. Example:
-// ...
-// hand 2 and han
-// handbag 5 and bag dba han ndb
-// handbook 6 and boo dbo han ndb ook
-
 #include <iostream>
 #include <fstream>
 #include <string>
--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -5,18 +5,36 @@
 #include <algorithm>
 #include "word.h"
 #include "dictionary.h"
+#include <unordered_set>
 
 using std::string;
 using std::vector;
 
-Dictionary::Dictionary() {
+// The constructor should read words.txt and
+// store the words in an unordered_set<std::string>.
+
+
+Dictionary::Dictionary()
+{
+	std::ifstream infile("words.txt");
+	if(!infile) {
+		std::cerr << "Error: coild not open wrods.txt" << std::endl;
+		return;
+	}
+	string s;
+	while (getline(infile, s))
+	{
+		words.insert(s.substr(0, s.find_first_of(" ")));
+	}
 }
 
-bool Dictionary::contains(const string& word) const {
-	return true;
+bool Dictionary::contains(const string &word) const
+{
+	return words.find(word) != words.end();
 }
 
-vector<string> Dictionary::get_suggestions(const string& word) const {
+vector<string> Dictionary::get_suggestions(const string &word) const
+{
 	vector<string> suggestions;
 	return suggestions;
 }
