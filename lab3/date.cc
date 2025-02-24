740866832a27d992f48cb1f1291508abae5bdd4c
Your Name <you@example.com>
2025-02-24 19:50:24 +0100

    commit

--- lab3/date.cc
+++ lab3/date.cc
@@ -1,11 +1,12 @@
-#include <ctime>  // time and localtime
+#include <ctime> // time and localtime
 #include "date.h"
 
 int Date::daysPerMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-Date::Date() {
-	time_t timer = time(0); // time in seconds since 1970-01-01
-	tm* locTime = localtime(&timer); // broken-down time
+Date::Date()
+{
+	time_t timer = time(0);			 // time in seconds since 1970-01-01
+	tm *locTime = localtime(&timer); // broken-down time
 	year = 1900 + locTime->tm_year;
 	month = 1 + locTime->tm_mon;
 	day = locTime->tm_mday;
@@ -13,28 +14,38 @@ Date::Date() {
 
 Date::Date(int y, int m, int d) : year(y), month(m), day(d) {}
 
-int Date::getYear() const {
+int Date::getYear() const
+{
 	return year;
 }
 
-int Date::getMonth() const {
+int Date::getMonth() const
+{
 	return month;
 }
 
-int Date::getDay() const {
+int Date::getDay() const
+{
 	return day;
 }
 
-void Date::next() {
-	if(daysPerMonth[month - 1] == day) {
+void Date::next()
+{
+	if (daysPerMonth[month - 1] == day)
+	{
 		day = 1;
-		if(month == 12) {
-			month= 1;
+		if (month == 12)
+		{
+			month = 1;
 			year++;
-		} else {
+		}
+		else
+		{
 			month++;
 		}
-	} else {
+	}
+	else
+	{
 		day++;
 	}
 }
